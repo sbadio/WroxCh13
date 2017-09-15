13181e2efbf034558d44f1ef253d254cb3af7304
sbadio <sergebadiojr@gmail.com>
2017-09-15 00:09:48 -0400

    Add project files.

---
+++ Management/Genres.aspx.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class Management_Genres
+    Inherits System.Web.UI.Page
+
+End Class
---
+++ MasterPages/Management.master.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class MasterPages_Management
+    Inherits System.Web.UI.MasterPage
+End Class
+
